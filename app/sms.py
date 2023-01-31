63a8231953ffede5898e46ec18822c0c6f8d5756
OChieng Otieno <ochiengotieno71@gmail.com>
2023-01-31 11:41:50 +0300

    add voice.py file

--- app/sms.py
+++ app/sms.py
@@ -16,7 +16,7 @@ def send_sms(phone: str, message: str):
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/x-www-form-urlencoded",
-        "apikey": API_KEY,
+        "apiKey": API_KEY,
     }
 
     res = requests.post(URL, data=data, headers=headers)
