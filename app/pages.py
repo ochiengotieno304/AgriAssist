1c23fe9a10965e4df77b1af7a8ba95c5469c9d64
Purity Muriungi <puritygatwiri1999@gmail.com>
2023-02-02 16:13:03 +0300

    added grants,specialists and subsidies files

--- app/pages.py
+++ app/pages.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, render_template, redirect, url_for
-from .models import User
+from .models import User,Grant,Specialist,Session,Subsidy
 
 from . import db 
 
@@ -26,3 +26,25 @@ def delete_user(id):
     db.session.delete(user)
     db.session.commit()
     return redirect(url_for('page.index'))
+
+@page.route('/update/<id>')
+def update_user(id):
+    user = User.query.filter_by(id=id).first_or_404()
+    db.session.update(user)
+    db.session.commit()
+    return redirect(url_for('page.index'))
+
+@page.route('/grants')
+def grants():
+    grants = Grant.query.all()
+    return render_template('grants.html',grants=grants)
+
+@page.route('/subsidies')
+def subsidies():
+    subsidies = Subsidy.query.all()
+    return render_template('subsidies.html',subsidies=subsidies)
+
+@page.route('/specialist')
+def specialists():
+    specialists= Specialist.query.all()
+    return render_template('specialists.html',specialists=specialists)
