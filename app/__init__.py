9b285cc8781eabaeedff884111db6bb5de300c53
Ochieng Otieno <ochiengotieno71@gmail.com>
2023-07-08 02:04:13 +0300

    fix: add http proxy to enable none-https session mp3 session recodings play

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, Response, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import requests
+import io
+import base64
 
 db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+
+    @app.context_processor
+    def my_context_processor():
+        def proxy(url):
+
+            response = requests.get(url,stream=True, proxies={"https": "https://3c5334842c52-15660798139000638402.ngrok-free.app"})
+            
+            # Check if the response was successful
+            if response.status_code == 200:
+                # Create a BytesIO object and write the content chunks to it
+                content_io = io.BytesIO()
+                for chunk in response.iter_content(chunk_size=1024):
+                    content_io.write(chunk)
+                content_io.seek(0)  # Move the file pointer to the beginning of the BytesIO object
+                return base64.b64encode(content_io.getvalue())
+            else:
+                return None
+
+
+        return {"proxy": proxy}
 
     app.config.from_pyfile('settings.py')
     app.config["FLASK_DEBUG"] = True
